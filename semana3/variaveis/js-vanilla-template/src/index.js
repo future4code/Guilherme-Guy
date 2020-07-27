3a3d684851f9431b01111ec0fe21003d31b9efa4
Guilherme Guy Louis <guigl8@hotmail.com>
2020-07-27 16:34:07 -0300

    pequena alteração

--- semana3/variaveis/js-vanilla-template/src/index.js
+++ semana3/variaveis/js-vanilla-template/src/index.js
@@ -107,11 +107,11 @@
 //4) 
 
 let perguntas = ["Voce gosta de frio? ", "Voce é mulher? ", "Sabe voar? "]
-let respostas = [true, false, false]
+let respostas = [true, false]
 
 console.log (perguntas[0], respostas[0]) 
 console.log (perguntas[1], respostas[1]) 
-console.log (perguntas[2], respostas[2]) 
+console.log (perguntas[2], respostas[0]) 
 
 
 
