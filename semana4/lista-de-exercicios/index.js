9c5dd037816cc53a3586ddfbef1fb7f2a5be5053
Guilherme Guy Louis <guigl8@hotmail.com>
2020-08-07 11:26:25 -0300

    Lista de exercicios

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,18 @@
+// Exercícios de interpretação de código
+
+// 1) Retorna o valor em reais de uma cotação passada em doolar. No caso, retornará o numero digitado * 100. 
+// Como: recebe a cotação em dolar pelo usuario via prompt e multiplica pelo parametro indicado pelo usuario, mas que no exercicio é 100.
+
+// 2) Ele calcuula o valor de cada investimento. Como ele faz: Ele pega o primeiro parametro indicado pelo usuário, verifica se está em algum dos cases,
+// se tiver mulplica pelo valor informado em cada investimento, caso não esteja em nenhum dos cases, retorna "tipo de investimento incorreto".
+// No console irá aparecer: 165 e "tipo de investimento incorreto" pois tesouno não existe na função. 
+
+// 3) Ele separa os numeros de um array, em novos arrays, um de pares e um de impares. 
+// Como faz: Se os numeros do array inicial forem dividios por 2 e o resto for 0, push noo array1. Caso contrário, push no array2.
+// No console irá aparecer: 1) a quantidade total de numeros: 14    2) 6   3)8 
+
+// 4) Ele pega o menor e o maior numero do array numeros. No caso seria exibido "-10" e "1590"
+
+
+
+
