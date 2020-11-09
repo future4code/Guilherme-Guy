6ccd6fee2eb8e71546dc81d4ac0db1b7f3b70856
GuilhermeGuyLouis <guigl8@hotmail.com>
2020-11-09 16:03:20 -0300

    aula1

---
+++ Semana18/Aula1/intro-autenticacao_implementacao/src/data/getUserByEmail.ts
@@ -0,0 +1,8 @@
+import { connection } from "..";
+export const getUserByEmail = async (email: string): Promise<any> => {
+    const result = await connection
+        .select("*")
+        .from("User")
+        .where({ email });
+    return result[0];
+}
